7ab97ff46bced992a689f32e2c5401cbe6040fdf
swfox <swfox@gmx.de>
2021-03-10 20:24:37 +0100

    add buildscript

---
+++ build.bash
@@ -0,0 +1,15 @@
+# run this script to build and deploy the webapp as a github page
+
+# build
+npm install
+npm run build
+
+# move files to pages folder
+cp -R ./public/ ./docs
+
+# push generated files to pages folder
+git add ./docs
+
+git commit -m "deploy page"
+git push
+
